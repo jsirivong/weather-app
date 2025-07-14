5ff432098afa8732c911d18ad9524b1377126791
pizza-eater-3500 <pizzaeater3500@gmail.com>
2025-07-14 19:46:58 -0400

    add input

---
+++ src/components/input.tsx
@@ -0,0 +1,7 @@
+export default function Input(){
+    return (
+        <form className="flex">
+            <input className="bg-white items-center text-center p-2 rounded-md" placeholder="Search for a city."/>
+        </form>
+    )
+}
