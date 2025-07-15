243db19c795dd6c9c8c96a087b2b37b1768a4a73
pizza-eater-3500 <pizzaeater3500@gmail.com>
2025-07-15 08:05:17 -0400

    link and picture credit for weatherapi

---
+++ src/components/weatherapicredit.tsx
@@ -0,0 +1,9 @@
+export default function WeatherAPICredit() {
+    return (
+        // work on weatherapicredit
+        <div className='inline-block text-center'>
+            <a href="https://www.weatherapi.com/" title="Free Weather API" className='text-white font-bold text-2xl'>Powered By WeatherAPI.com</a>
+            <img src='//cdn.weatherapi.com/v4/images/weatherapi_logo.png' alt="Weather data by WeatherAPI.com" />
+        </div>
+    )
+}
