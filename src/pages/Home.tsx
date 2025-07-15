918050e855ae069fcfa06a0ee17857c488df2450
pizza-eater-3500 <pizzaeater3500@gmail.com>
2025-07-14 23:24:18 -0400

    create home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,17 @@
-export default function Home(){
+import '../App.css'
+import Input from '../components/input';
+import Popup from'../components/popup';
+
+export default function Home() {
     return (
         <div>
-            
+            <h2 className='text-center text-white font-bold text-6xl pt-25'>Weather App</h2>
+            <p className='text-center text-white font-bold text-xl pt-3'>The look out for all your plans.</p>
+
+            <div>
+                <Input />
+                <Popup />
+            </div>
         </div>
     )
 }
