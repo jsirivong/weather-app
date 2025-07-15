a4b1d3f40c49b93d5927e47a9176d7224ea75034
pizza-eater-3500 <pizzaeater3500@gmail.com>
2025-07-15 06:16:14 -0400

    add functionality to input bar

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,7 +1,43 @@
+import { useEffect, useState, type FormEvent } from "react"
+import { useNavigate } from 'react-router-dom'
+import getWeatherData from "../services/api";
+import type CurrentWeatherData from "../types/WeatherData";
+
 export default function Input(){
+    const [ input, setInput ] = useState("");
+    const navigate = useNavigate()
+
+    const handleWeather = async (data: any): Promise<void> => {
+        const weatherData: CurrentWeatherData = await data;  
+
+        if (weatherData) {
+            setInput("");
+            navigate("/weather", {state: {data: weatherData}})
+        } else {
+            return
+        }
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+        e.preventDefault()
+
+        if (!input.trim()){
+            return
+        }
+
+        const data = getWeatherData({city: input, api_method: "/current.json"})
+
+        handleWeather(data)
+    }
+    
+    useEffect(() => {
+        
+    }, [input])
+
     return (
-        <form className="flex items-center">
-            <input className="bg-white items-center p-2 rounded-md" placeholder="Search for a city."/>
+        <form className="flex items-center" onSubmit={handleSubmit}>
+            <input value={input} onChange={(e) => setInput(e.target.value)} className="bg-white items-center p-2 rounded-md mx-auto w-xl translate-y-20 shadow-emerald-500 shadow-xl text-gray-600 hover:bg-gray-300" placeholder="Search for a city."/>
+            <button type="submit" className="bg-white shadow-xl shadow-emerald-500 -translate-95 relative rounded px-4 py-1.5 translate-y-20 text-gray-400 hover:bg-gray-300">Search</button>
         </form>
     )
-}
+}  
