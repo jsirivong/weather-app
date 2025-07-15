554189793d3499fbfcf835ff419b464d788ca187
pizza-eater-3500 <pizzaeater3500@gmail.com>
2025-07-14 23:22:34 -0400

    add routing to different pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import './App.css'
-import Input from './components/input';
+import Home from "./pages/Home";
 
 export default function App(){
   return (
-    <div>
-      <h2 className='text-center text-white font-bold text-6xl pt-25'>Weather App</h2>
-      <p className='text-center text-white font-bold text-xl pt-3'>The look out for all your plans.</p>
-
-      <div>
-        <Input/>
-      </div>
-    </div>
+    <Home/>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// the RouterProvider is a react-router component
+// the RouterProvider takes the router object we defined
+import Weather from './pages/Weather.tsx'
+import NotFound from './components/notfound.tsx'
+
+const router = createBrowserRouter([
+  { path: "/", element: <App/> },  // index or home page of our application, the element is the component is will render at that path
+  { path: "/weather", element: <Weather/>},
+  { path: "*", element: <NotFound/>} // path: "*" handles any URL that isn't explicitly stated in our createBrowserRouter setup
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router}/> 
+  </StrictMode>
 )
