cfb4a01e2b082d6c517264f0bac379e2acd3110c
pizza-eater-3500 <pizzaeater3500@gmail.com>
2025-07-15 22:20:20 -0400

    got rid of nested <a> tag

--- src/components/weatherapicredit.tsx
+++ src/components/weatherapicredit.tsx
@@ -1,10 +1,10 @@
 export default function WeatherAPICredit() {
     return (
         <div className='text-center absolute bottom-0 left-1/2 -translate-x-1/2 my-5'>
-            <a href="https://www.weatherapi.com/" title="Free Weather API" className='text-white font-bold text-lg'>Powered By Weather.com</a>
-            <a href="https://www.weatherapi.com/" title="Free Weather API">
+            <span className="text-white font-bold text-lg">Powered by WeatherAPI</span>
+            <a href="https://www.weatherapi.com/" title="Free Weather API" className='text-white font-bold text-lg'>
                 <img src='//cdn.weatherapi.com/v4/images/weatherapi_logo.png' alt="Weather data by WeatherAPI.com" className="mx-auto"/>
             </a>
         </div>
     )
-}
+}   
